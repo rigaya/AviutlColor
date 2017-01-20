ad28f0c6e6905e2fb99eb05d60db669612d22604
rigaya <rigaya34589@live.jp>
2017-01-20 20:19:53 +0900

    利用可能なSIMD命令を検出するコードを追加。

---
+++ common/color_simd.cpp
@@ -0,0 +1,58 @@
+﻿// -----------------------------------------------------------------------------------------
+// AviutlColor by rigaya
+// -----------------------------------------------------------------------------------------
+// The MIT License
+//
+// Copyright (c) 2017 rigaya
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// --------------------------------------------------------------------------------------------
+
+#include <cstdint>
+#include "color_simd.h"
+
+#include <intrin.h>
+
+uint32_t get_availableSIMD() {
+    int CPUInfo[4];
+    __cpuid(CPUInfo, 1);
+    uint32_t simd = NONE;
+    if (CPUInfo[3] & 0x04000000) simd |= SSE2;
+    if (CPUInfo[2] & 0x00000001) simd |= SSE3;
+    if (CPUInfo[2] & 0x00000200) simd |= SSSE3;
+    if (CPUInfo[2] & 0x00080000) simd |= SSE41;
+    if (CPUInfo[2] & 0x00100000) simd |= SSE42;
+    if (CPUInfo[2] & 0x00800000) simd |= POPCNT;
+#if (_MSC_VER >= 1600)
+    uint64_t xgetbv = 0;
+    if ((CPUInfo[2] & 0x18000000) == 0x18000000) {
+        xgetbv = _xgetbv(0);
+        if ((xgetbv & 0x06) == 0x06)
+            simd |= AVX;
+    }
+#endif
+#if (_MSC_VER >= 1700)
+    __cpuid(CPUInfo, 7);
+    if ((simd & AVX) && (CPUInfo[1] & 0x00000020))
+        simd |= AVX2;
+#endif
+    return simd;
+}
+
---
+++ common/color_simd.h
@@ -0,0 +1,56 @@
+﻿// -----------------------------------------------------------------------------------------
+// AviutlColor by rigaya
+// -----------------------------------------------------------------------------------------
+// The MIT License
+//
+// Copyright (c) 2017 rigaya
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// --------------------------------------------------------------------------------------------
+
+#ifndef __COLOR_SIMD_H__
+#define __COLOR_SIMD_H__
+
+#include <cstdint>
+
+enum : uint32_t {
+    NONE   = 0x0000,
+    SSE2   = 0x0001,
+    SSE3   = 0x0002,
+    SSSE3  = 0x0004,
+    SSE41  = 0x0008,
+    SSE42  = 0x0010,
+    POPCNT = 0x0020,
+    AVX    = 0x0040,
+    AVX2   = 0x0080,
+};
+
+struct convert_color_func {
+    convert_func yuy2_yc48;
+    convert_func yc48_yuy2;
+    convert_func yc48_btxxx_bt601;
+    convert_func yc48_bt601_btxxx;
+};
+
+void get_func(convert_color_func *func_list);
+
+uint32_t get_availableSIMD();
+
+#endif //__COLOR_SIMD_H__
